bd335fd71f5e874102b5b18840d7db4fddf3b9cd
ibraheemkabir <ibraheemkabir96@gmail.com>
2018-07-19 22:38:47 +0100

    babel configuration done@

--- Api.js
+++ Api.js
@@ -1,43 +1,16 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+
+const routes = require('./routes/UserRoute');
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const Users = [
-  { id: 1, Username: 'kabir', Password: 'kabir' },
-  { id: 2, Username: '5', Password: 'entry' },
-];
+app.use('/', routes);
 
-app.get('/users', (req, res) => {
-  res.send(Users);
-});
-
-app.post('users/signup', (req, res) => {
-  if (!req.body.Username || !req.body.Password) {
-    res.json({ success: false, msg: 'Please pass username and password.' });
-  } else {
-    const newUser = {
-      id: Users.lenght + 1,
-      Username: req.body.Username,
-      Password: req.body.Password,
-    };
-    // save the user
-    Users.push(newUser);
-    res.send(Users);
-  }
-});
-
-app.post('users/signin', (req, res) => {
-  const users = Users.find(Y => Y.Username === req.body.Username);
-  if (!users) res.status(404).send('The user is invalid');
-  else if (users.Password === req.body.Password) {
-    res.send('welldone');
-  } else {
-    res.send('what');
-    //  res.status(401).send({ success: false, msg: 'Authentication failed. Wrong password.' });
-  }
-});
 
 const port = process.env.Port || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
