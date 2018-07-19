9f4321363b1ef74b6fce112d32c0d86d90f42ebd
ibraheemkabir <ibraheemkabir96@gmail.com>
2018-07-19 22:59:35 +0100

    finished babel config

--- Api.js
+++ Api.js
@@ -1,52 +1,16 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+
+const routes = require('./Routes/EntryRoute');
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const entries = [
-  {
-    id: 1, entry: 'ui', title: 'entry', Uid: 1,
-  },
-  {
-    id: 2, entry: '5', title: 'entry', Uid: 2,
-  },
-];
+app.use('/', routes);
 
-app.get('/entries', (req, res) => {
-  res.send(entries);
-});
-
-app.get('entries/:id', (req, res) => {
-  const arr = entries.find(c => c.id === parseInt(req.params.id, 10));
-  if (!arr) res.status(404).send('The entry is invalid');
-  res.send(arr);
-});
-
-app.post('entries/', (req, res) => {
-  const enter = {
-    id: entries.lenght + 1,
-    name: req.body.name,
-    Title: req.body.title,
-  };
-
-  entries.push(enter);
-  res.send(entries);
-});
-
-app.put('entries/:id', (req, res) => {
-  const arr = entries.find(c => c.id === parseInt(req.params.id, 10));
-  if (!arr) res.status(404).send('The entry is invalid');
-  arr.name = req.body.name;
-  arr.title = req.body.title;
-  res.send(arr);
-});
-
-app.delete('entries/:id', (req, res) => {
-  const arr = entries.find(c => c.id === parseInt(req.params.id, 10));
-  entries.pop(arr);
-  res.send(arr);
-});
 
 const port = process.env.Port || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- Routes/EntryRoute.js
+++ Routes/EntryRoute.js
@@ -4,7 +4,7 @@ import User from '../models/Entry';
 const router = express.Router();
 
 const entries = [
-  new User('James Coonce', 'jcoonce', 'none@none.com', 'real'),
+  new User('first', 'my trip was cool', '29-3-2000', 21),
   new User('Bob Coonce', 'bcoonce', 'none@none.com', 'real'),
   new User('Euri', 'euri', 'none@none.com', 'real'),
   new User('Norman', 'jcoonce', 'none@none.com', 'real'),
@@ -14,13 +14,13 @@ router.get('/entries', (req, res) => {
   res.send(entries);
 });
 
-router.get('entries/:id', (req, res) => {
+router.get('/entries/:id', (req, res) => {
   const arr = entries.find(c => c.id === parseInt(req.params.id, 10));
   if (!arr) res.status(404).send('The entry is invalid');
   res.send(arr);
 });
 
-router.post('entries/', (req, res) => {
+router.post('/entries/', (req, res) => {
   const enter = {
     id: entries.lenght + 1,
     name: req.body.name,
@@ -31,7 +31,7 @@ router.post('entries/', (req, res) => {
   res.send(entries);
 });
 
-router.put('entries/:id', (req, res) => {
+router.put('/entries/:id', (req, res) => {
   const arr = entries.find(c => c.id === parseInt(req.params.id, 10));
   if (!arr) res.status(404).send('The entry is invalid');
   arr.name = req.body.name;
@@ -39,7 +39,7 @@ router.put('entries/:id', (req, res) => {
   res.send(arr);
 });
 
-router.delete('entries/:id', (req, res) => {
+router.delete('/entries/:id', (req, res) => {
   const arr = entries.find(c => c.id === parseInt(req.params.id, 10));
   entries.pop(arr);
   res.send(arr);
