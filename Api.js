78f67392b70b1d505994a8aa8ad977ff3d0d507b
ibraheemkabir <ibraheemkabir96@gmail.com>
2018-07-20 04:44:33 +0100

    set up coverage

--- Api.js
+++ Api.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', routes);
+app.use('/api/v1', routes);
 
 
 const port = process.env.Port || 3000;
