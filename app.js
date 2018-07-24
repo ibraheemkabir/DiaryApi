d190fb9df17f124570f899a13bcac7145fcad887
ibraheemkabir <ibraheemkabir96@gmail.com>
2018-07-25 00:25:36 +0100

    nyc

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/v1/entry', routes);
 
-const port = process.env.Port || 3000;
+const port = process.env.Port || 7000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = app;
