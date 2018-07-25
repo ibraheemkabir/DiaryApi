6ecbc9027981ee029e0fc346a7532fe8089ba0fd
ibraheemkabir <ibraheemkabir96@gmail.com>
2018-07-25 19:45:33 +0100

    add

--- routes/route.js
+++ routes/route.js
@@ -25,4 +25,13 @@ router.post('/', (req, res) => {
   res.send(entries);
 });
 
+
+router.put('/:id', (req, res) => {
+  const arr = entries.find(c => c.id === parseInt(req.params.id, 10));
+  if (!arr) res.status(404).send('The entry is invalid');
+  arr.name = req.body.name;
+  arr.title = req.body.title;
+  res.send(arr);
+});
+
 module.exports = router;
