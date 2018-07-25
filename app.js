302b1df5ac5676aa8ea62e2e42cca962295c43b7
ibraheemkabir <ibraheemkabir96@gmail.com>
2018-07-25 22:22:52 +0100

    added root page content

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const home = require('./server/routes/index');
 const add = require('./server/routes/addRoute');
 const del = require('./server/routes/deleteEntry');
 const all = require('./server/routes/getallRoute');
 const getentry = require('./server/routes/getidRoute');
 const Update = require('./server/routes/updateEntry');
 
+app.use('/', home);
 app.use('/api/v1/entry/', add);
 app.use('/api/v1/entry/', del);
 app.use('/api/v1/entry/', all);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
 
+const router = express.Router();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'index' });
+router.get('/', (req, res) => {
+  res.send('Welcome to mydiary rest API');
 });
-
-router.get
 
 module.exports = router;
