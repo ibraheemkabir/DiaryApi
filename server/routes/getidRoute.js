43c41172f3041b7b409744de2f4e613f84145e7d
ibraheemkabir <ibraheemkabir96@gmail.com>
2018-07-26 01:31:08 +0100

    changed file structure

--- server/routes/getidRoute.js
+++ server/routes/getidRoute.js
@@ -1,15 +1,7 @@
 import express from 'express';
-import User from '../models/entrymodel';
+import entries from '../helpers/array';
 
 const router = express.Router();
-const datetime = require('node-datetime');
-
-const id = 1;
-const dt = datetime.create();
-const formatted = dt.format('m/d/Y');
-const entries = [
-  new User(id, 'Dear Diary My first post', 'my trip was cool', formatted),
-];
 
 router.get('/:id', (req, res) => {
   const arr = entries.find(c => c.id === parseInt(req.params.id, 10));
